d5c1d22be209dbc926fde1f3d118ee12754aa9b5
adamyajain <apoorvadoc@gmail.com>
2020-09-01 18:39:16 +0530

    commited

---
+++ config.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCD9whKNN_pKiKKOI-xEQoHZ4z9kyjVtKg",
+    authDomain: "wily-ab6a5.firebaseapp.com",
+    databaseURL: "https://wily-ab6a5.firebaseio.com",
+    projectId: "wily-ab6a5",
+    storageBucket: "wily-ab6a5.appspot.com",
+    messagingSenderId: "1039620996208",
+    appId: "1:1039620996208:web:5f2681985123ae868c88b5",
+    measurementId: "G-DLHQPP4NV5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, ToastAndroid } from 'react-native';
 import * as Permissions from 'expo-permissions'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 
@@ -44,6 +44,72 @@ export default class BookTransactionScreen extends React.Component {
         });
       }
   }
+
+  initiateBookIssue = async()=>{
+    db.collection("transaction").add({
+      'studentId' : this.state.scannedStudentId,
+      'bookId' : this.state.scannedBookId,
+      'data' : firebase.firestore.Timestamp.now().toDate(),
+      'transactionType' : "Issue"
+    })
+
+    db.collection("books").doc(this.state.scannedBookId).update({
+      'bookAvailability' : false
+    })
+    db.collection("students").doc(this.state.scannedStudentId).update({
+      'bookAvailability' : false
+    })
+
+    this.setState()({
+      scannedBookId: '',
+      scannedStudentId: ''
+    })
+  }
+
+  initiateBookReturn = async() =>{
+    db.collection("transaction").add({
+      'studentId' : this.state.scannedStudentId,
+      'bookId' : this.state.scannedBookId,
+      'date' : firebase.firestore.Timestamp.now().toDate(),
+      'transactionType' : "Return"
+    })
+
+    db.collection("books").doc(this.state.scannedBookId).update({
+      'bookAvailability' : true
+    })
+
+    db.collection("students").doc(this.state.scannedStudentId).udate({
+      'numberOfBooksIssued' : firebase.firestore.fieldValue.increment(-1)
+    })
+
+    this.setState({
+      scannedBookId: '',
+      scannedStudentId: ''
+    })
+  }
+
+  handleTransaction = async() =>{
+    var transactionMessage = null;
+    db.collection("books").doc(this.state.scannedBookId).get()
+    .then((doc)=>{
+      var book = doc.data()
+      if(book.bookAvailability){
+        this.initiateBookIssue();
+        transactionMessage = "Book Issued"
+        ToastAndroid.show(transactionMessage, ToastAndroid.SHORT)
+      }
+      else{
+        this.initiateBookReturn();
+        transactionMessage = "Book Returned"
+        ToastAndroid.show(transactionMessage, ToastAndroid.SHORT)
+      }
+    })
+
+    this.setState({
+      transactionMessage : transactionMessage
+    })
+  }
+  
     render() {
       const hasCameraPermissions = this.state.hasCameraPermissions;
       const scanned = this.state.scanned;
@@ -60,7 +126,10 @@ export default class BookTransactionScreen extends React.Component {
 
       else if (buttonState === "normal"){
         return(
-          <View style={StyleSheet.container}>
+            <KeyboardAvoidingView 
+            style={styles.container}
+            behaviour="padding" enabled
+            >
             <View>
               <Image
               source={require("../assets/booklogo.jpg")}
@@ -71,6 +140,7 @@ export default class BookTransactionScreen extends React.Component {
               <TextInput
               style={styles.inputBox}
               placeholder="Book Id"
+              onChangeText={text =>this.setState({scannedBookId:text})}
               value={this.state.scannedBookId}/>
             <TouchableOpacity
             style={styles.scanButton}
@@ -84,6 +154,7 @@ export default class BookTransactionScreen extends React.Component {
               <TextInput
               style={styles.inputBox}
               placeholder="Student Id"
+              onChangeText={text =>this.setState({scannedStudentId:text})}
               value={this.state.scannedStudentId}
               />
               <TouchableOpacity
@@ -94,7 +165,17 @@ export default class BookTransactionScreen extends React.Component {
                 <Text style={styles.buttonText}>Scan</Text>
               </TouchableOpacity>
               </View>
-            </View>
+              <TouchableOpacity
+               styles={styles.submitButton}
+               onPress={async()=>{
+                 var transactionMessage = this.habdleTransaction();
+                 this.setState(
+                   {scannedBookId: '',
+                  scannedStudentId:''})
+               }}>
+                 <Text style={styles.submitButtonText}>Submit</Text>
+               </TouchableOpacity>
+              </KeyboardAvoidingView>
         );
       }
     }
@@ -134,5 +215,17 @@ export default class BookTransactionScreen extends React.Component {
       width: 50,
       borderWidth: 1.5,
       borderLeftWidth: 0
+    },
+    submitButton:{
+      backgroundColor: '#FBC02D',
+      width: 100,
+      height: 50
+    },
+    submitButtonTetx:{
+      padding: 10,
+      textAlign: 'center',
+      fontSize: 20,
+      fontWeight: "bold",
+      color: 'white'
     }
   });
